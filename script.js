de972d0a5c534fd7c757eeea68a2eb0efe431008
Edouard Misset <edouardmisset@gmail.com>
2021-02-11 14:34:09 +0100

    working with city names and states search + highlight

--- script.js
+++ script.js
@@ -2,43 +2,54 @@ const endpoint =
   "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
 const cities = [];
 const resultList = document.querySelector(".suggestions");
+const input = document.querySelector(".search");
 
-document.querySelector("input.search").addEventListener("keyup", displayResult);
-document
-  .querySelector("input.search")
-  .addEventListener("change", displayResult);
+input.addEventListener("change", displayResult);
+input.addEventListener("keyup", displayResult);
 
 fetch(endpoint)
   .then((res) => res.json())
-  .then((obj) => obj.forEach((city) => cities.push(city)))
+  .then((data) => cities.push(...data))
   .catch((err) => console.warn(err));
 
 // Filter
-const filter = (arr = [], query = "") => {
-  const reg = new RegExp(query, "gi");
-  return arr.filter((item) => {
-    let flag = false;
-    for (prop in item) {
-      if (reg.test(item[prop])) {
-        flag = true;
-      }
-    }
-    return flag;
+const findQuery = (query, cities = []) => {
+  const regex = new RegExp(query, "gi");
+  return cities.filter(({ city, state }) => {
+    return city.match(regex) || state.match(regex);
   });
 };
 
 // Display result
 function displayResult() {
-  console.log(this.value);
-  console.log(filter(cities, this.value));
+  const matchedQuery = findQuery(this.value, cities);
 
-  while (resultList.firstChild) {
-    resultList.removeChild(resultList.firstChild);
-  }
+  const html = matchedQuery
+    .map(({ city, state, population }) => {
+      const regex = RegExp(this.value, "gi"),
+        cityName = city.replace(regex, `<span class="hl">${this.value}</span>`),
+        stateName = state.replace(
+          regex,
+          `<span class="hl">${this.value}</span>`
+        );
+      return `
+    <li>
+      <span class="name">${cityName}, ${stateName}</span>
+      <span class="name">${population}</span>
+    </li>
+  `;
+    })
+    .join("");
 
-  return filter(cities, this.value).forEach((city) => {
-    newLi = document.createElement("li");
-    newLi.innerHTML = city.city;
-    resultList.appendChild(newLi);
-  });
+  // while (resultList.firstChild) {
+  //   resultList.removeChild(resultList.firstChild);
+  // }
+
+  // return matchedQuery.forEach((city) => {
+  //   newLi = document.createElement("li");
+  //   newLi.innerHTML = city.city;
+  //   resultList.appendChild(newLi);
+  // });
+
+  resultList.innerHTML = html;
 }
