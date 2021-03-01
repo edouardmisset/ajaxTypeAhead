842a57613fa9bbb75e6183a235fd26d28fa7ae84
Edouard Misset <edouardmisset@gmail.com>
2021-03-01 15:24:07 +0100

    cleanup

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const endpoint =
-  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+  'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 const cities = [];
-const resultList = document.querySelector(".suggestions");
-const input = document.querySelector(".search");
+const resultList = document.querySelector('.suggestions');
+const input = document.querySelector('.search');
 
-input.addEventListener("change", displayResult);
-input.addEventListener("keyup", displayResult);
+input.addEventListener('change', displayResult);
+input.addEventListener('keyup', displayResult);
 
 fetch(endpoint)
   .then((res) => res.json())
@@ -14,7 +14,7 @@ fetch(endpoint)
 
 // Filter
 const findQuery = (query, cities = []) => {
-  const regex = new RegExp(query, "gi");
+  const regex = new RegExp(query, 'gi');
   return cities.filter(({ city, state }) => {
     return city.match(regex) || state.match(regex);
   });
@@ -22,7 +22,7 @@ const findQuery = (query, cities = []) => {
 
 // Format numbers
 const formatNumber = (someNumber) => {
-  return someNumber.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  return someNumber.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 };
 
 // Display result
@@ -31,7 +31,7 @@ function displayResult() {
 
   const html = matchedQuery
     .map(({ city, state, population }) => {
-      const regex = RegExp(this.value, "gi"),
+      const regex = RegExp(this.value, 'gi'),
         cityName = city.replace(regex, `<span class="hl">${this.value}</span>`),
         stateName = state.replace(
           regex,
@@ -44,17 +44,7 @@ function displayResult() {
     </li>
   `;
     })
-    .join("");
-
-  // while (resultList.firstChild) {
-  //   resultList.removeChild(resultList.firstChild);
-  // }
-
-  // return matchedQuery.forEach((city) => {
-  //   newLi = document.createElement("li");
-  //   newLi.innerHTML = city.city;
-  //   resultList.appendChild(newLi);
-  // });
+    .join('');
 
   resultList.innerHTML = html;
 }
